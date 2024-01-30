b81feb0ccc3da1e8d5e1fc89b72928ec0c67c7bf
ga63yir <anna.haas@tum.de>
2024-01-30 10:28:47 +0100

    geometry preparation

---
+++ vessel_graph_generation/geometries/lungmask.py
@@ -0,0 +1,22 @@
+import os
+import glob
+from multiprocessing import Pool
+from tqdm import tqdm
+
+def process_file(path):
+    filename = os.path.basename(path)
+    filename = f"{filename.split('.')[0]}_lobes_mask.nii.gz"
+    # output_path = os.path.join("/home/ahaas/data/0_input_simulation/ATM_preparation/masks_refined", filename)
+    output_path = os.path.join("/home/shared/Data/ATM22/train/masks", filename)
+    if os.path.exists(output_path):
+        print(f"Skipping {path}, already exists")
+        return
+    os.system(f"lungmask --modelname LTRCLobes_R231 --batchsize 16 {path} {output_path} --noprogress >/dev/null 2>&1")
+
+if __name__ == "__main__":
+    input_dir = "/home/shared/Data/ATM22/train/images"
+    files = glob.glob(os.path.join(input_dir, "*"))
+
+    with Pool(5) as p, tqdm(total=len(files), desc='Lobe masks in progress...') as pbar:
+        for _ in p.imap_unordered(process_file, files):
+            pbar.update()
